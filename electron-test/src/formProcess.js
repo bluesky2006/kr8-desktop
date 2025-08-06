94a932e5f2e8ecae5b6ec9fc1d775d40adedd0c6
bluesky2006 <simon.busby@gmail.com>
2025-08-06 15:46:28 +0100

    refactor parsing utils to work in chain with uploaded m3u file

--- electron-test/src/formProcess.js
+++ electron-test/src/formProcess.js
@@ -1,23 +1,28 @@
-export const form = document.getElementById('form');
-form.addEventListener('submit', async (e) => {
+import { extractTrackPaths } from "./utils/extractTrackPaths";
+import { getParsedTrackMetadata } from "./utils/getParsedTrackMetadata";
+
+export const form = document.getElementById("form");
+form.addEventListener("submit", async (e) => {
   try {
     e.preventDefault();
-    const input = document.getElementById('file-input');
+    const input = document.getElementById("file-input");
     const data = input.files[0];
     const file = await data.text();
 
-    console.log(file, 'file data');
     // call the manipulation functions
+    const trackPathArray = await extractTrackPaths(file);
+    console.log(trackPathArray);
 
-    
+    const object = await getParsedTrackMetadata(trackPathArray);
+    console.log(object);
+
     //return object with image data
   } catch (err) {
     console.log(err);
   }
 
-  console.log('form was submitted');
+  console.log("form was submitted");
 });
-
 
 //render here
 
