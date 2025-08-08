28b1ac653d930f8d64f3e77942a0fc3bfcb5f8fc
bluesky2006 <simon.busby@gmail.com>
2025-08-08 11:47:57 +0100

    copy additional info from track metadata into desired object

--- electron-test/src/utils/metadataParser.js
+++ electron-test/src/utils/metadataParser.js
@@ -3,19 +3,24 @@
 export async function metadataParser(filePath, index) {
   try {
     const metadata = await window.electronAPI.parseMetadata(filePath);
-    
+    console.log(metadata, "metadata object");
+
     return {
       track_id: index + 1,
-      track_artist: metadata.common.artist || 'Unknown Artist',
-      track_title: metadata.common.title || 'Unknown Title',
+      track_artist: metadata.common.artist || "Unknown Artist",
+      track_title: metadata.common.title || "Unknown Title",
+      track_bpm: metadata.common.bpm || "",
+      track_length: metadata.format.duration,
       track_image: metadata.common.picture?.[0]?.data ?? null,
     };
   } catch (error) {
     console.error(`Error parsing metadata for ${filePath}:`, error.message);
     return {
       track_id: index + 1,
-      track_artist: 'Unknown Artist',
-      track_title: 'Unknown Title',
+      track_artist: "Unknown Artist",
+      track_title: "Unknown Title",
+      track_bpm: "",
+      track_length: 0,
       track_image: null,
     };
   }
