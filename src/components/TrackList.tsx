303b02e61d2e79bfbce03096d868f3ff51832fbd
bluesky2006 <simon.busby@gmail.com>
2025-09-30 16:26:23 +0100

    rename index variable

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -7,8 +7,8 @@ interface TrackListProps {
 export function TrackList({ tracks }: TrackListProps) {
   return (
     <section className="mt-2 w-full space-y-6">
-      {tracks.map((track, i) => (
-        <TrackCard key={i} track={track} />
+      {tracks.map((track, index) => (
+        <TrackCard key={index} track={track} />
       ))}
     </section>
   );
