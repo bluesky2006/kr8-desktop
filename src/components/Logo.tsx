31eb3f9c372e5f96bcc92b1cbc41e636f669091d
bluesky2006 <simon.busby@gmail.com>
2025-10-01 10:52:27 +0100

    tweak bottom margin

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export function Logo({ compact }: { compact?: boolean }) {
   return (
     <h1
       id="main_title"
-      className={`font-bold font-rubik-80s text-red-400 transition-all duration-300 ${compact ? "mb-8 text-6xl" : "mb-36 text-8xl"}`}
+      className={`font-bold font-rubik-80s text-red-400 transition-all duration-300 ${compact ? "mb-8 text-6xl" : "mb-40 text-8xl"}`}
     >
       kr8
     </h1>
