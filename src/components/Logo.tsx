d123503bf400212d287e136eb62c1c4f654d8bf8
bluesky2006 <simon.busby@gmail.com>
2025-10-01 10:17:29 +0100

    add animation to logo resizing transition

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export function Logo({ compact }: { compact?: boolean }) {
   return (
     <h1
       id="main_title"
-      className={`font-bold text-red-400 ${compact ? "mb-8 text-4xl" : "mb-36 text-8xl"}`}
+      className={`font-bold text-red-400 transition-all duration-300 ${compact ? "mb-8 text-4xl" : "mb-36 text-8xl"}`}
     >
       kr8
     </h1>
