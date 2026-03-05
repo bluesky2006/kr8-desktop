16c072538e4d79d8eeb8ad0022c638141f90528b
bluesky2006 <simon.busby@gmail.com>
2026-03-05 13:57:46 +0000

    enhance TrackList component with track manipulation functionalities

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,11 +1,20 @@
 import { TrackCard } from "./TrackCard.js";
 import { TrackListProps } from "../types/types.js";
 
-export function TrackList({ tracks }: TrackListProps) {
+export function TrackList({ tracks, onMoveTrack, onDeleteTrack, onToggleFavourite }: TrackListProps) {
   return (
     <section className="mt-2 w-full space-y-6">
       {tracks.map((track, index) => (
-        <TrackCard key={index} track={track} />
+        <TrackCard
+          key={index}
+          track={track}
+          isFirst={index === 0}
+          isLast={index === tracks.length - 1}
+          onMoveUp={() => onMoveTrack(index, index - 1)}
+          onMoveDown={() => onMoveTrack(index, index + 1)}
+          onToggleFavourite={() => onToggleFavourite(index)}
+          onDelete={() => onDeleteTrack(index)}
+        />
       ))}
     </section>
   );
